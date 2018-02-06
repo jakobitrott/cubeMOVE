3a5eb68461b3a5e43fb99f6312e8d90dfbd3961c
jakobisaac <jakobtrott@gmail.com>
2018-02-06 03:14:14 +0000

    commit 3
    - Added proper scoring

--- Assets/Scripts/cubeMovement.cs
+++ Assets/Scripts/cubeMovement.cs
@@ -1,22 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class cubeMovement : MonoBehaviour {
 
 	// Use this for initialization
 	public float z;
 	Vector3 moveZ;
+	public TextMeshProUGUI text;
+	private int score = 1;
 
-	public int changeSpeed = 3;
+	public float changeSpeed = 3.0f;
 	
 	// Update is called once per frame
 	void Update () {
-
+		//changeSpeed = changeSpeed + Time.deltaTime;
 		z = transform.position.z;
 		z++;
 		moveZ = new Vector3 (transform.position.x, transform.position.y, z);
 			transform.position = Vector3.MoveTowards(transform.position,moveZ,changeSpeed* Time.deltaTime);
 
+	}
+	void OnTriggerEnter(Collider other) {
+		text.text = score.ToString ();	
+		++score;
 	}
 }
---
+++ Assets/Scripts/scoreCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class scoreCollision : MonoBehaviour {
+	public TextMeshProUGUI text;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("jesus");
+		text.text = score.ToString ();	
+		++score;
+	}
+	}
