7558a8918c72e001ef66d321f5c169e45c680260
jakobisaac <jakobtrott@gmail.com>
2018-02-06 02:37:47 +0000

    Added Endless Platforms
    - Added endless Platforms
    - bug fixes

--- Assets/Scripts/platformGenerator.cs
+++ Assets/Scripts/platformGenerator.cs
@@ -5,32 +5,57 @@ using UnityEngine;
 public class platformGenerator : MonoBehaviour {
 
 
-	public float distanceBetween;
 	private float platformWidth;
-	public static int maxPlatforms = 5;
 	private int platforms = 0;
-	public GameObject[] thePlatform = new GameObject[maxPlatforms];
+	private int maxPlatforms = 5;
+	//public GameObject[] thePlatform = new GameObject[maxPlatforms];
+	 List<GameObject> thePlatform;
+	public GameObject platform;
+	public GameObject player;
 	Vector3 newVector;
 
 	// Use this for initialization
 	void Start () {
+		//player = new GameObject ();
+		thePlatform = new List<GameObject>();
+		for (int i = 0; i < 5; i++) {
+			Debug.Log ("bokb");
+			thePlatform.Add (Instantiate (platform as GameObject, newVector,transform.rotation));
+			updateGeneratorPosition ();
+		}
+			
+			
+		}
 
+	void Update(){
+		if (player.transform.position.z - thePlatform [0].transform.position.z > 20) {
+			GameObject objectdestroy = thePlatform [0];
+			thePlatform.Remove (objectdestroy);
+			Destroy (objectdestroy);
+			--platforms;
+
+			while (platforms != maxPlatforms) {
+
+				thePlatform.Add (Instantiate (platform as GameObject, newVector,transform.rotation));
+				updateGeneratorPosition ();
+			
+			}
+		}
+
+
+	}
+
+	void updateGeneratorPosition(){
+
+
+		newVector = transform.position;
+		newVector = new Vector3(transform.position.x,transform.position.y, transform.position.z + 19.815f);
+		transform.position = newVector;
+		++platforms;
+
+	}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (platforms <= maxPlatforms) {
 
-			newVector = transform.position;
 
-			Instantiate (thePlatform[Random.Range(0,4)], newVector, transform.rotation);
-
-			newVector = new Vector3(transform.position.x,transform.position.y, transform.position.z + 19.815f);
-
-			transform.position = newVector;
-
-			++platforms;
-
-	}
-}
-}
---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class score : MonoBehaviour {
+
+	public Transform player;
+	public TextMeshProUGUI text;
+
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = player.position.z.ToString ("0");
+	}
+}
